5c429347bb043db0867d889c97b6b3c6a9db03cb
Shane <shanedelatorre2412@gmail.com>
2025-03-03 19:35:12 +0800

    Integrate add-task to tasks component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,10 +3,11 @@ import { Task } from '../../Task';
 import { CommonModule } from '@angular/common';
 import { TaskItemComponent } from '../task-item/task-item.component';
 import { TaskService } from '../../services/task.service';
+import { AddTaskComponent } from '../add-task/add-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [CommonModule, TaskItemComponent ],
+  imports: [CommonModule, TaskItemComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -30,6 +31,10 @@ export class TasksComponent implements OnInit {
     task.reminder = !task.reminder;
     this.taskService.updateTaskReminder(task)
     .subscribe();
+  }
+
+  addTask(task: Task){ 
+    
   }
 
 }
