1d1e88a0e8e8ca5ff5324cc0019f051dabf18a32
Shane <shanedelatorre2412@gmail.com>
2025-03-03 19:34:42 +0800

    Update add-task now functionable

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
-export class AddTaskComponent {
 
+export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+  taskName?: string;
+  day?: string;
+  reminder: boolean = false;
+
+
+  onSubmit() {
+    if (!this.taskName) {
+      alert('Please add a task');
+      return;
+    }
+
+    const newTask: Task = {
+      text: this.taskName || '',
+      day: this.day || '',
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+    this.taskName = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
